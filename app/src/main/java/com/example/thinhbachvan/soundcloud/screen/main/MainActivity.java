7abbb1f7b02eb6a46db6abedd0fbf9b197a1d059
Bach Van Thinh <thinh.bachvan@nccsoft.office>
2018-09-26 13:21:41 +0700

    creating layout for main activity and home fragment

--- app/src/main/java/com/example/thinhbachvan/soundcloud/screen/main/MainActivity.java
+++ app/src/main/java/com/example/thinhbachvan/soundcloud/screen/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.thinhbachvan.soundcloud;
+package com.example.thinhbachvan.soundcloud.screen.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.thinhbachvan.soundcloud.R;
 
 public class MainActivity extends AppCompatActivity {
 
